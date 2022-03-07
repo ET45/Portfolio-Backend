c87e9e815ffd441307819c005bf509424259087c
ET45 <emre.tugutlu@gmail.com>
2022-03-07 18:41:12 +0300

    DetailPage added

--- routers/character.js
+++ routers/character.js
@@ -5,8 +5,22 @@ const Character = require("../models/").character;
 const router = new Router();
 
 router.get("/characters", async (req, res) => {
-  const allCharacters = await Character.findAll();
-  res.status(200).send(allCharacters);
+  try {
+    const allCharacters = await Character.findAll();
+    res.status(200).send(allCharacters);
+  } catch (e) {
+    console.log("error", e);
+  }
+});
+
+router.get("/characters/:id", async (req, res) => {
+  try {
+    const characterId = parseInt(req.params.id);
+    const character = await Character.findByPk(characterId);
+    res.status(200).send(character);
+  } catch (e) {
+    console.log("error", e);
+  }
 });
 
 module.exports = router;
